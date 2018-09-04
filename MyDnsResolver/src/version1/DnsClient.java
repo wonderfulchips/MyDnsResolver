2b503898fe623f759b2777970ba241c6057a5509
吴雨晨 <291149763@qq.com>
2018-09-04 11:50:53 +0800

    初步建立连接：使用UDP协议使得客户端想服务器查询主机，服务器能返回其全部ip

--- MyDnsResolver/src/version1/DnsClient.java
+++ MyDnsResolver/src/version1/DnsClient.java
@@ -1,5 +1,35 @@
 package version1;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
 public class DnsClient {
+	public static void main(String[] args) throws IOException {
+		DatagramSocket clientSocket=new DatagramSocket();
+		
+		Scanner sc=new Scanner(System.in);
+		String hostName=null;
+		System.out.print("请输入查询主机:");
+		hostName=sc.nextLine();
+		//input the query name
+		
+		byte[] data=new byte[100];
+		data=hostName.getBytes();
+		DatagramPacket sendPacket= new DatagramPacket(data,hostName.length(),InetAddress.getByName("127.0.0.1"),12345);
+		clientSocket.send(sendPacket);
+		System.out.println("已发送查询请求");
+		
+		byte[] buf=new byte[100];
+		DatagramPacket packet =new DatagramPacket(buf, buf.length);
+		clientSocket.receive(packet);
+		//receive server's answer
+		
+		String received=new String(packet.getData(),0,packet.getLength());
+		System.out.println("结果如下：");
+		System.out.println(received);
+	}
 
 }
--- MyDnsResolver/src/version1/DnsServer.java
+++ MyDnsResolver/src/version1/DnsServer.java
@@ -1,5 +1,40 @@
 package version1;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class DnsServer {
+	public static void main(String[] args) throws IOException {
+		DatagramSocket serverSocket= new DatagramSocket(12345);
+		//create serversocket on port 12345, using UDP protocol
+		
+		byte buf[]= new byte [100];
+		DatagramPacket receivePacket = new DatagramPacket(buf,buf.length);
+		System.out.println("DNSServer is started, ready for query...");
+		System.out.println("-----------------------------------------------");
+		while(true) {
+			serverSocket.receive(receivePacket);
+			String str=new String(receivePacket.getData(),0,receivePacket.getLength());
+			System.out.println("Inquire the ip address of "+str+":");
+			//receive client's DNS query		
+			
+			InetAddress[] addresses = InetAddress .getAllByName(str); 
+			//System.out.println(addresses.length); 
+			String answer=new String();
+			for (InetAddress addr : addresses) { 
+				System.out.println(addr.getHostAddress()); 
+				answer=answer+"/"+addr.getHostAddress();
+			} 
+			//use array for a host may have multiple ip address 
+			
+			byte[] data=new byte[100];
+			data=answer.getBytes();
+			DatagramPacket sendPacket= new DatagramPacket(data,answer.length(),receivePacket.getAddress(),receivePacket.getPort());
+			serverSocket.send(sendPacket);
+			System.out.print("ÒÑ·¢ËÍ»Ø¸´");
+
+		}
+	}
 
 }
