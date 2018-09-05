d0b3c22cbe855349c652b7f150826c6acc9ac71c
吴雨晨 <291149763@qq.com>
2018-09-05 10:38:40 +0800

    完善了退出机制/增加了异常处理
    客户端使用".quit"正常退出;服务器接收到".shutdown"正常关闭。
    客户端设置了超时退出，5秒内未收到回复就程序正常退出。
    服务器若未解析域名成功就抛出异常并反馈给客户端输入域名不正确。

--- MyDnsResolver/src/version1/DnsClient.java
+++ MyDnsResolver/src/version1/DnsClient.java
@@ -1,44 +1,74 @@
 package version1;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DnsClient {
+	public static boolean running=true;
 	public static void main(String[] args) throws IOException {
 		DatagramSocket clientSocket=new DatagramSocket();
 		
 		Scanner sc=new Scanner(System.in);
 		String hostName=null;
-		System.out.print("请输入查询主机:");
-		hostName=sc.nextLine();
-		//input the query name
-		
-		byte[] data=new byte[200];
-		data=hostName.getBytes();
-		DatagramPacket sendPacket= new DatagramPacket(data,hostName.length(),InetAddress.getByName("127.0.0.1"),12345);
-		clientSocket.send(sendPacket);
-		System.out.println("已发送查询请求");
-		
-		byte[] buf=new byte[200];
-		DatagramPacket packet =new DatagramPacket(buf, buf.length);
-		clientSocket.receive(packet);
-		//receive server's answer
-		
-		String received=new String(packet.getData(),0,packet.getLength());
-		if (received.substring(received.length()-1,received .length()).equals("/")){
-			//to check if we get the right resolved ip
-			System.out.println(hostName+"的查询结果如下：");
-			String [] str0 =received.split("/");
-			for (String s :str0) {
-				System.out.println(s);
+		while(running) {
+			System.out.print("请输入查询主机:");
+			hostName=sc.nextLine();
+			//input the query name
+			
+			if(!(hostName.equalsIgnoreCase(".quit")||hostName.equalsIgnoreCase(".shutdown"))) {
+				byte[] data=new byte[200];
+				data=hostName.getBytes();
+				DatagramPacket sendPacket= new DatagramPacket(data,hostName.length(),InetAddress.getByName("127.0.0.1"),12345);
+				clientSocket.send(sendPacket);
+				System.out.println("已发送查询请求");
+				
+				Timer timer=new Timer();
+				timer.schedule(new TimerTask() {		
+					@Override
+					public void run() {
+						System.out.println("请求超时，客户端已退出。");
+						System .exit(0);
+					}
+				}, 5000);
+				//若在5秒内未接收到回复，则认为超时并且结束这个客户端。
+				
+				byte[] buf=new byte[200];
+				DatagramPacket packet =new DatagramPacket(buf, buf.length);
+				clientSocket.receive(packet);
+				//receive server's answer
+				
+				String received=new String(packet.getData(),0,packet.getLength());
+				if (received.substring(received.length()-1,received .length()).equals("/")){
+					//to check if we get the right resolved ip
+					System.out.println(hostName+"的查询结果如下：");
+					String [] str0 =received.split("/");
+					for (String s :str0) {
+						System.out.println(s);
+					}
+				}
+				else {
+					//hostname query is forbidden
+					System.out.println(received);
+				}
 			}
-		}
-		else {
-			//hostname query is forbidden
-			System.out.println(received);
+			else {
+				if(hostName.equalsIgnoreCase(".shutdown")) {
+					byte[] data=new byte[200];
+					data=hostName.getBytes();
+					DatagramPacket sendPacket= new DatagramPacket(data,hostName.length(),InetAddress.getByName("127.0.0.1"),12345);
+					clientSocket.send(sendPacket);
+				}
+				clientSocket.close();
+				running =false;
+				System.out.println("客户端已退出");
+			}
 		}
 		
 	}
--- MyDnsResolver/src/version1/DnsServer.java
+++ MyDnsResolver/src/version1/DnsServer.java
@@ -6,6 +6,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class DnsServer {
+	public static boolean running=true;
 	public static void main(String[] args) throws IOException {
 		DatagramSocket serverSocket= new DatagramSocket(12345);
 		//create serversocket on port 12345, using UDP protocol
@@ -13,39 +14,53 @@ public class DnsServer {
 		byte buf[]= new byte [200];
 		DatagramPacket receivePacket = new DatagramPacket(buf,buf.length);
 		System.out.println("DNSServer is started, ready for query...");
-		System.out.println("-----------------------------------------------");
-		while(true) {
+		while(running) {
+			System.out.println("-----------------------------------------------");
 			serverSocket.receive(receivePacket);
 			String str=new String(receivePacket.getData(),0,receivePacket.getLength());
-			System.out.println("Inquire the ip address of "+str+":");
-			//receive client's DNS query		
-			
-			InetAddress[] addresses = InetAddress .getAllByName(str); 
-			//System.out.println(addresses.length); 
-			String answer=new String();
-			for (InetAddress addr : addresses) { 
-				System.out.println(addr.getHostAddress()); 
-				answer=addr.getHostAddress()+"/"+answer;
-			} 
-			//use array for a host may have multiple ip address 
-			
-			byte[] data=new byte[200];
-			
-			if(testForbidden(str)==false) {
-				data=answer.getBytes();
-				DatagramPacket sendPacket= new DatagramPacket(data,answer.length(),receivePacket.getAddress(),receivePacket.getPort());
-				serverSocket.send(sendPacket);
-				System.out.print("已发送回复");
+			if(!str.equalsIgnoreCase(".shutdown")) {
+				System.out.println("Inquire the ip address of "+str+":");
+				//receive client's DNS query		
+				byte[] data=new byte[200];
+				
+				try {
+					InetAddress[] addresses = InetAddress .getAllByName(str); 			
+					//使用java自带的接口函数获得解析后的ip
+					String answer=new String();
+					for (InetAddress addr : addresses) { 
+						System.out.println(addr.getHostAddress()); 
+						answer=addr.getHostAddress()+"/"+answer;
+					} 
+					//use array for a host may have multiple ip address 
+					
+					if(testForbidden(str)==false) {
+						data=answer.getBytes();
+						DatagramPacket sendPacket= new DatagramPacket(data,answer.length(),receivePacket.getAddress(),receivePacket.getPort());
+						serverSocket.send(sendPacket);
+						System.out.print("已发送回复");
+					}
+					else {
+						String noAnswer ="not found!";//中文字符传递客户端只能收到前三个？
+						data=noAnswer.getBytes();
+						DatagramPacket sendPacket= new DatagramPacket(data,noAnswer.length(),receivePacket.getAddress(),receivePacket.getPort());
+						serverSocket.send(sendPacket);
+						System.out.println("client查找"+str+"的ip讯息已被屏蔽");
+					}
+				} catch (Exception e) {
+					//如果getallbyname()无法解析该域名则解析失败
+					System.out.println("解析域名失败!");
+					String noAnswer ="please input the correct domain name！";//中文字符传递客户端只能收到前三个？
+					data=noAnswer.getBytes();
+					DatagramPacket sendPacket= new DatagramPacket(data,noAnswer.length(),receivePacket.getAddress(),receivePacket.getPort());
+					serverSocket.send(sendPacket);
+				}
 			}
 			else {
-				String noAnswer ="not found!";//中文字符传递客户端只能收到前三个？
-				data=noAnswer.getBytes();
-				DatagramPacket sendPacket= new DatagramPacket(data,noAnswer.length(),receivePacket.getAddress(),receivePacket.getPort());
-				serverSocket.send(sendPacket);
-				System.out.println("client查找"+str+"的ip讯息已被屏蔽");
+				running =false;
+				serverSocket .close();
+				System.out.println("服务器关闭。");
 			}
 		}
-		//serverSocket.close();
 	}
 	public static boolean testForbidden(String str) {
 		boolean flag = false;
